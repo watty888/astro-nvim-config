138747157f1b41482ad2d2c6657737a4bf3853b7
Elcin Bunyatov <aono_88@hotmail.com>
2026-03-04 22:20:57 +0100

    Added 10 lines scrolloff for better ui

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -45,6 +44,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        scrolloff = 10, -- sets vim.opt.scrolloff
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
